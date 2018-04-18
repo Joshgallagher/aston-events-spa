8d7f509f55619105af3e1d2f65a89eff17ad808c
Joshua Gallagher <joshgallagher24@gmail.com>
2018-04-18 15:42:38 +0100

    Started signup page styling

--- src/app/auth/pages/AuthSignup.vue
+++ src/app/auth/pages/AuthSignup.vue
@@ -1,5 +1,27 @@
 <template>
-  <h1>Signup</h1>
+  <div class="section">
+    <div class="container is-fluid"></div>
+    <div class="columns">
+      <div class="column is-half is-offset-one-quarter">
+        <h1 class="title">Join Today.</h1>
+        <p class="subtitle">Create, favorite and follow events happening at Aston!</p>
+        <b-field label="Full Name">
+          <b-input type="text"></b-input>
+        </b-field>
+        <b-field label="Email Address">
+          <b-input type="email"></b-input>
+        </b-field>
+        <b-field label="Contact Number">
+          <b-input type="tel" maxlength="11"></b-input>
+        </b-field>
+        <b-field label="Password">
+          <b-input type="password"></b-input>
+        </b-field>
+        <a class="button is-primary is-medium is-fullwidth">Create Account</a>
+        <p class="has-account has-text-centered">Have an account? <a href="#" class="has-text-link">Login</a></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +30,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="sass">
+.has-account
+  margin: 20px 0
 </style>
