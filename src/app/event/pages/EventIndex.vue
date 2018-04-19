7fec87da2def62e3dc65a56720e7b1be9a6cb77d
Joshua Gallagher <joshgallagher24@gmail.com>
2018-04-19 22:22:06 +0100

    Started working on Event Index - specifically the Event item

--- src/app/event/pages/EventIndex.vue
+++ src/app/event/pages/EventIndex.vue
@@ -1,5 +1,52 @@
 <template>
-  <h1>Event Index</h1>
+  <div>
+    <div class="section">
+      <div class="container is-fluid">
+        <div class="columns">
+          <div class="column is-6">
+
+            <div class="box">
+              <div class="columns">
+                <div class="column is-11">
+                  <a href="#" class="title is-5">
+                    <span class="is-7 has-text-primary has-text-weight-normal">Joshua Gallagher organised</span>
+                    Aston Tigers (American Football)
+                  </a>
+                </div>
+                <div class="column is-1 has-text-right">
+                  <b-icon
+                    pack="mdi"
+                    icon="heart"
+                    size="is-small"
+                    type="is-danger">
+                  </b-icon>
+                  10
+                </div>
+              </div>
+              <div class="columns">
+                <div class="date-column column">
+                  <b-icon
+                    pack="mdi"
+                    icon="clock"
+                    size="is-small">
+                  </b-icon>
+                  <span>12th June 2018 @ 1:00PM</span>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ipsum deserunt, quia consectetur? Maxime accusantium neque dignissimos, assumenda tenetur delectus reiciendis voluptate eaque quos repellat nisi necessitatibus odio, officia aliquam.
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +55,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="sass">
+.date-column
+  padding-top: 0
 </style>
