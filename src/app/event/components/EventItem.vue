e981e4399c69130cfe0507f6a54e973385f48ac5
Joshua Gallagher <joshgallagher24@gmail.com>
2018-04-20 02:34:51 +0100

    Moved event item to a component

---
+++ src/app/event/components/EventItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-11">
+        <a href="#" class="title is-5">
+          <span class="is-7 has-text-primary has-text-weight-normal">Joshua Gallagher organised</span>
+          Aston Tigers (American Football)
+        </a>
+      </div>
+      <div class="column is-1 has-text-right">
+        <b-icon
+          pack="mdi"
+          icon="heart"
+          size="is-small"
+          type="is-danger">
+        </b-icon>
+        10
+      </div>
+    </div>
+    <div class="columns">
+      <div class="date-column column">
+        <b-icon
+          pack="mdi"
+          icon="clock"
+          size="is-small">
+        </b-icon>
+        <span>12th June 2018 @ 1:00PM</span>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ipsum deserunt, quia consectetur? Maxime accusantium neque dignissimos, assumenda tenetur delectus reiciendis voluptate eaque quos repellat nisi necessitatibus odio, officia aliquam.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'event-item'
+}
+</script>
+
+<style lang="sass">
+</style>
--- src/app/event/pages/EventIndex.vue
+++ src/app/event/pages/EventIndex.vue
@@ -3,48 +3,10 @@
     <div class="section">
       <div class="container is-fluid">
         <div class="columns">
-          <div class="column is-6">
-          <b-notification type="is-warning" v-if=user v-show=!confirmed>
-            Hey <strong>{{ user.name }}</strong>! You still need to confirm your email address before you can organise events on AstonEvents!
-          </b-notification>
-
-            <div class="box">
-              <div class="columns">
-                <div class="column is-11">
-                  <a href="#" class="title is-5">
-                    <span class="is-7 has-text-primary has-text-weight-normal">Joshua Gallagher organised</span>
-                    Aston Tigers (American Football)
-                  </a>
-                </div>
-                <div class="column is-1 has-text-right">
-                  <b-icon
-                    pack="mdi"
-                    icon="heart"
-                    size="is-small"
-                    type="is-danger">
-                  </b-icon>
-                  10
-                </div>
-              </div>
-              <div class="columns">
-                <div class="date-column column">
-                  <b-icon
-                    pack="mdi"
-                    icon="clock"
-                    size="is-small">
-                  </b-icon>
-                  <span>12th June 2018 @ 1:00PM</span>
-                </div>
-              </div>
-              <div class="columns">
-                <div class="column">
-                  <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ipsum deserunt, quia consectetur? Maxime accusantium neque dignissimos, assumenda tenetur delectus reiciendis voluptate eaque quos repellat nisi necessitatibus odio, officia aliquam.
-                  </div>
-                </div>
-              </div>
-            </div>
-
+          <div class="column is-8">
+            <b-notification type="is-warning" v-if="user" v-show="!confirmed">
+              Hey <strong>{{ user.name }}</strong>! You still need to confirm your email address before you can organise events on AstonEvents!
+            </b-notification>
           </div>
         </div>
       </div>
@@ -55,8 +17,14 @@
 <script>
 import { mapGetters } from 'vuex'
 
+import eventItem from '../components/EventItem'
+
 export default {
   name: 'event-index',
+
+  components: {
+    eventItem
+  },
 
   computed: {
     ...mapGetters({
