dd6131f2325c61621dac57c0b6c2639114e01d52
Joshua Gallagher <joshgallagher24@gmail.com>
2018-04-21 19:49:01 +0100

    Added icons to global menu fiters - removed account option

--- src/app/global/components/GlobalMenu.vue
+++ src/app/global/components/GlobalMenu.vue
@@ -1,20 +1,64 @@
 <template>
   <aside class="menu">
     <p class="menu-label">
-      My Account
-    </p>
-    <ul class="menu-list">
-      <li><a>My Events</a></li>
-      <li><a>My Favorites</a></li>
-      <li><a>Log Out</a></li>
-    </ul>
-    <p class="menu-label">
       Choose a Filter
     </p>
     <ul class="menu-list">
-      <li><a>All Events</a></li>
-      <li><a>Events Today</a></li>
-      <li><a>Popular Events</a></li>
+      <li>
+        <a>
+          <b-icon
+            pack="mdi"
+            icon="chart-bubble"
+            size="is-small"
+            type="is-primary">
+          </b-icon>
+          All Events
+        </a>
+      </li>
+      <li>
+        <a>
+          <b-icon
+            pack="mdi"
+            icon="pencil"
+            size="is-small"
+            type="is-primary">
+          </b-icon>
+          My Events
+        </a>
+      </li>
+      <li>
+        <a>
+          <b-icon
+            pack="mdi"
+            icon="heart"
+            size="is-small"
+            type="is-primary">
+          </b-icon>
+          My Favorites
+        </a>
+      </li>
+      <li>
+        <a>
+          <b-icon
+            pack="mdi"
+            icon="calendar-check"
+            size="is-small"
+            type="is-primary">
+          </b-icon>
+          Today
+        </a>
+      </li>
+      <li>
+        <a>
+          <b-icon
+            pack="mdi"
+            icon="fire"
+            size="is-small"
+            type="is-primary">
+          </b-icon>
+          Popular
+        </a>
+      </li>
     </ul>
     <p class="menu-label">
       Or Pick a Channel
