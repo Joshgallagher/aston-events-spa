fba69cc68ba6f6dc9951a7e8f0210ee215780661
Joshua Gallagher <joshgallagher24@gmail.com>
2018-04-21 19:30:17 +0100

    Added page specification option to getEvents action

--- src/app/event/store/actions.js
+++ src/app/event/store/actions.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import * as mutationTypes from './mutationTypes'
 
-export const getEvents = ({ dispatch, commit }) => {
-  return Vue.axios.get('http://aston-events-api.test/api/v1/events')
-    .then((res) => {
-      commit(mutationTypes.SET_EVENTS_DATA, res.data.data)
-      commit(mutationTypes.SET_EVENTS_LINKS, res.data.links)
-      commit(mutationTypes.SET_EVENTS_META, res.data.meta)
-    })
+export const getEvents = ({ dispatch, commit }, page = 1) => {
+  return Vue.axios.get('http://aston-events-api.test/api/v1/events', {
+    params: {
+      page
+    }
+  }).then((res) => {
+    commit(mutationTypes.SET_EVENTS_DATA, res.data.data)
+    commit(mutationTypes.SET_EVENTS_LINKS, res.data.links)
+    commit(mutationTypes.SET_EVENTS_META, res.data.meta)
+  })
 }
